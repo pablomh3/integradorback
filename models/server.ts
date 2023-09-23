7b018a8b3c064de0102c4865c14a078457ced5a6
pablomh3 <pablonudenberg@gmail.com>
2023-09-23 16:52:30 -0300

    cors

--- models/server.ts
+++ models/server.ts
@@ -28,9 +28,9 @@ export class Server {
     
     middlewares(): void {
         this.app.use((req, res, next) => {
-            res.setHeader('Access-Control-Allow-Origin', 'https://integrador-react-peach.vercel.app');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', '*');
+            res.setHeader('Access-Control-Allow-Headers', '*');
             next();
         });
         
