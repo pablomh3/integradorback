b179561dbc7f159673db173ec84d62a3aa9dab48
pablomh3 <pablonudenberg@gmail.com>
2023-09-23 17:19:36 -0300

    cors

--- models/server.ts
+++ models/server.ts
@@ -31,7 +31,6 @@ export class Server {
             res.setHeader('Access-Control-Allow-Origin', 'https://integrador-react-peach.vercel.app');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token');
-            res.sendStatus(204)
             next();
         });
         
